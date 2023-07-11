df8f3fc3404b1a02ed31aed5824cf6ce1409bf44
AmerSouccar <114993993+AmerSouccar@users.noreply.github.com>
2023-07-11 14:15:18 +0200

    Updates

--- src/TaskLog.Application/TaskManagement/Phases/Dto/CreatePhaseDto.cs
+++ src/TaskLog.Application/TaskManagement/Phases/Dto/CreatePhaseDto.cs
@@ -10,6 +10,8 @@ namespace TaskLog.TaskManagement.Phases.Dto
     {
         public string Title { get; set; }
         public string Description { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
         public int ProjectId { get; set; }
     }
 }
